fa9ef8fe1893a3eeb8bb700c1819225c086c2893
mtthw <matbalinton@gmail.com>
2025-09-09 10:01:13 +0800

    fixed the localhost issue

--- src/components/citizen/partials/ChatBot.tsx
+++ src/components/citizen/partials/ChatBot.tsx
@@ -66,7 +66,7 @@ export default function ChatBot() {
   }
 
   useEffect(() => {
-    const newSocket = io('http://localhost:5000', {
+    const newSocket = io(`${import.meta.env.VITE_API_URL}`, {
       transports: ['websocket'],
       reconnection: true,
       reconnectionAttempts: 5,
